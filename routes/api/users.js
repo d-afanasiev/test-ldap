4158fbea3367646f95c0ed76c351e633829504ec
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2022-10-04 21:45:06 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,25 +23,6 @@ router.post("/login", function (req, res) {
     return res.json(list);
   });
 
-  // ad.authenticate(username, password, function (err, auth) {
-  //   if (err) {
-  //     return res.json("Authentication failed!");
-  //   }
-
-  //   if (auth) {
-  //     console.log(auth);
-  //     return res.json(req.body);
-  //     ad.isUserMemberOf(username, "101-DG-Room", function (err, isMember) {
-  //       if (err) {
-  //         console.log("ERROR: " + JSON.stringify(err));
-  //         return;
-  //       }
-  //       console.log(username + " isMemberOf " + groupName + ": " + isMember);
-  //     });
-  //   } else {
-  //     return res.json("Authentication failed!");
-  //   }
-  // });
 });
 
 module.exports = router;
